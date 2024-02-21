a8e8fb46a3a0670c66a274b8d7dccd6f46340673
Danilo <danilocampos388@gmail.com>
2024-02-20 21:55:04 -0300

    component parameters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Formulario />
     </div>
   );
 }
